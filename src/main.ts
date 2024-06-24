af345c50ff3fc4546cfb0d456a2e645481cccaf7
Tomhuel <tomasnahuelantela@gmail.com>
2024-06-24 22:35:20 +0100

    Swagger & dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  app.setGlobalPrefix('api/v1');
+  const config = new DocumentBuilder()
+    .setTitle('Genshin API')
+    .setDescription('Complete Genshin API')
+    .setVersion('1.0')
+    .addTag('Genshin')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
   await app.listen(3000);
 }
 
